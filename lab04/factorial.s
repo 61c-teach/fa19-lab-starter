ce72e49de8147d8c21cbc72d5235533ee0805f1d
Sayan Paul <sayanpaul@berkeley.edu>
2019-09-20 01:25:59 -0700

    Add newline in factorial

--- lab04/factorial.s
+++ lab04/factorial.s
@@ -6,11 +6,15 @@ main:
     la t0, n
     lw a0, 0(t0)
     jal ra, factorial
-    
+
     addi a1, a0, 0
     addi a0, x0, 1
     ecall # Print Result
-    
+
+    addi a1, x0, '\n'
+    addi a0, x0, 11
+    ecall # Print newline
+
     addi a0, x0, 10
     ecall # Exit
 
